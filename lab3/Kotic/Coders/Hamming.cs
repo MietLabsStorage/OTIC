e053160cd9d236104e44cf7f958e09551bff6c84
mJaJksJ <maksim.m00@mail.ru>
2021-12-10 13:11:39 +0300

    hamming encode. nedd fill info

--- lab3/Kotic/Coders/Hamming.cs
+++ lab3/Kotic/Coders/Hamming.cs
@@ -1,39 +1,45 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using Kotic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Test
+namespace Kotic.Coders
 {
-    class Program
+    public class Hamming: ICoder
     {
-        static void Main(string[] args)
+        public byte[] Decode(byte[] file, byte[] info, int oldSize)
         {
-            var file = BitConverter.GetBytes(123456);
+            List<byte> blob = new List<byte>();
+
+            return blob.ToArray();
+        }
+
+        public (byte[] blob, byte[] info) Encode(byte[] file)
+        {
+            List<byte> blob = new List<byte>();
+            List<byte> info = new List<byte>() { 0x00, 0x00 };
+
             var bitsArr = new BitArray(file);
             var inputBits = new List<bool>();
-            foreach(var bit in bitsArr)
+            foreach (var bit in bitsArr)
             {
                 inputBits.Add((bool)bit);
             }
 
-            //
-            inputBits = new List<bool>() { true, false, false, true, false, false, true, false, true, true, true, false, false, false, true };
-            //
-
             var controlCount = (int)Math.Ceiling(Math.Log2(inputBits.Count + 1)) + 1;
-            for(int i = 1; i <= controlCount; i++)
+            for (int i = 1; i <= controlCount; i++)
             {
                 inputBits.Insert((int)(Math.Pow(2, i - 1) - 1), false);
             }
 
             var table = new bool[controlCount, inputBits.Count];
 
-            for(int i = 0; i < controlCount; i++)
+            for (int i = 0; i < controlCount; i++)
             {
                 var id = 0;
-                while(id < (int)Math.Pow(2, i) - 1)
+                while (id < (int)Math.Pow(2, i) - 1)
                 {
                     table[i, id] = false;
                     id++;
@@ -44,7 +50,7 @@ namespace Test
                 {
                     for (int k = 0; k < (int)Math.Pow(2, i); k++)
                     {
-                        if(id >= inputBits.Count)
+                        if (id >= inputBits.Count)
                         {
                             doing = false;
                             break;
@@ -70,7 +76,7 @@ namespace Test
             for (int i = 0; i < controlCount; i++)
             {
                 var res = 0;
-                for(int j = 0; j < inputBits.Count; j++)
+                for (int j = 0; j < inputBits.Count; j++)
                 {
                     res += (inputBits[j] ? 1 : 0) * (table[i, j] ? 1 : 0);
                 }
@@ -79,8 +85,19 @@ namespace Test
 
             for (int i = 1; i <= controlCount; i++)
             {
-                inputBits[(int)(Math.Pow(2, i - 1) - 1)] = controlLine[i-1];
+                inputBits[(int)(Math.Pow(2, i - 1) - 1)] = controlLine[i - 1];
             }
+
+            var listTable = new List<bool>();
+            for(int i = 0; i < table.Length; i++)
+            {
+                listTable.Add(table[i / inputBits.Count, i % inputBits.Count]);
+            }
+            var bitTable = new BitArray(listTable.ToArray());
+            byte[] byteTable = new byte[bitTable.Length / 8 + 1];
+            bitTable.CopyTo(byteTable, 0);
+
+            return (blob.ToArray(), info.ToArray());
         }
     }
 }
